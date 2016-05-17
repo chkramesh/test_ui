685174e53a31feaf9c96e13bbd5c6db641b9406e
chkramesh <chkramesh@gmail.com>
2016-05-17 13:53:47 -0400

    Create FoodRecipeConfiguration.java

---
+++ src/main/java/com/foodsense/configuration/FoodRecipeConfiguration.java
@@ -0,0 +1,36 @@
+package com.foodsense.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com")
+//@ComponentScan(basePackages = "com.foodsense")
+public class FoodRecipeConfiguration extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+		registry.addResourceHandler("/Content/**").addResourceLocations("/Content/");
+		registry.addResourceHandler("/Scripts/**").addResourceLocations("/Scripts/");
+		registry.addResourceHandler("/Employee/**").addResourceLocations("/Employee/");
+		registry.addResourceHandler("/Pages/**").addResourceLocations("/Pages/");
+		registry.addResourceHandler("/Templates/**").addResourceLocations("/Templates/");
+	}
+}
